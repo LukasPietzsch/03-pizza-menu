a426381d6bc66482a2caf92c3eb95affc1cb8cfa
Lukas Pietzsch <Lukas.Pietzsch@btb.gmbh>
2026-08-04 15:22:40 +0200

    Chapter 36: Multiple Disgusting Pizzas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import pizzaData from "./data";
+
 function App() {
     return (
-        <h1>Hello World!</h1>
+        <div>
+            <h1>Hello World!</h1>
+            <Pizza />
+            <Pizza />
+            <Pizza />
+        </div>
+    );
+}
+
+function Pizza() {
+    return (
+        <div>
+            <img src="pizzas/spinaci.jpg" alt = "Pizza spinaci" />
+            <h2>Pizza Pizza!</h2>
+            <p>Tomato</p>
+        </div>
     );
 }
 
